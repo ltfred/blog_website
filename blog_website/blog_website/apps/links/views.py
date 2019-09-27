a39f9a66306125dea883dba51b527e30b4c63d37
ltfred <ltfred@163.com>
2019-09-27 15:33:42 +0800

    soup修改

--- blog_website/blog_website/apps/links/views.py
+++ blog_website/blog_website/apps/links/views.py
@@ -20,15 +20,14 @@ class LinkView(View):
             recommend_links = Link.objects.filter(is_recommend=True)
             links = Link.objects.filter(is_recommend=False)
             # 随机一句心灵鸡汤
-            content = ''
             soups = Soup.objects.all()
-            if soups:
-                soup = random.choice(soups)
-                content = soup.content
         except Exception as e:
             logger.error(e)
             return http.HttpResponse('获取界面失败')
-
-        context = {'recommend_links': recommend_links, 'links': links, 'soup': content}
+        context = {
+            'recommend_links': recommend_links,
+            'links': links,
+            'soup': random.choice(soups).content if soups else ''
+        }
 
         return render(request, 'daohang.html', context=context)

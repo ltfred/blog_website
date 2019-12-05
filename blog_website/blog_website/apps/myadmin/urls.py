5693421e1612a95ed54b1942fb7e651460463f4d
Fred <liutao@fred.local>
2019-12-05 14:36:38 +0800

    首页数据统计

--- blog_website/blog_website/apps/myadmin/urls.py
+++ blog_website/blog_website/apps/myadmin/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
-from myadmin.views import users
+from myadmin.views import users, statistical
 from myadmin.views.users import AdminUserView
 
 
 urlpatterns = [
     url(r'^authorizations/$', users.AdminAuthorizeView.as_view()),
+    url(r'^statistical/user_count/$', statistical.AdminUserCount.as_view()),
+    url(r'^statistical/article_count/$', statistical.AdminArticleCount.as_view()),
+    url(r'^statistical/photo_count/$', statistical.AdminPhotoCount.as_view()),
+    url(r'^statistical/label_count/$', statistical.AdminLabelCount.as_view()),
+    url(r'^statistical/day_active/$', statistical.AdminUserDayActiveCount.as_view()),
+    url(r'^statistical/month_increment/$', statistical.AdminArticleMonthCountView.as_view()),
+    url(r'^statistical/label_article/$',statistical.AdminLabelArticle.as_view()),
 ]
 
 router = DefaultRouter()
---
+++ blog_website/blog_website/apps/myadmin/views/statistical.py
@@ -0,0 +1,108 @@
+from datetime import timedelta
+from django.utils import timezone
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from article.models import Article, Label
+from blog_website.utils.common import get_labels
+from photo.models import Photo
+from user.models import User
+
+
+class AdminUserCount(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+        count = User.objects.count()
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        response_data = {
+            'date': now_date.date(),
+            'count': count
+        }
+
+        return Response(response_data)
+
+
+class AdminArticleCount(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+        count = Article.objects.count()
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        response_data = {
+            'date': now_date.date(),
+            'count': count
+        }
+
+        return Response(response_data)
+
+
+class AdminPhotoCount(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+        count = Photo.objects.count()
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        response_data = {
+            'date': now_date.date(),
+            'count': count
+        }
+
+        return Response(response_data)
+
+
+class AdminLabelCount(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+        count = Label.objects.count()
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        response_data = {
+            'date': now_date.date(),
+            'count': count
+        }
+
+        return Response(response_data)
+
+
+class AdminUserDayActiveCount(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        count = User.objects.filter(last_login__gte=now_date).count()
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        response_data = {
+            'date': now_date.date(),
+            'count': count
+        }
+        return Response(response_data)
+
+
+class AdminArticleMonthCountView(APIView):
+    permission_classes = [IsAdminUser]
+    def get(self, request):
+
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        begin_date = now_date - timedelta(days=29)
+        count_list = []
+        for i in range(30):
+            cur_date = begin_date + timedelta(days=i)
+            next_date = cur_date + timedelta(days=1)
+            count = Article.objects.filter(create_time__gte=cur_date, create_time__lt=next_date).count()
+            count_list.append({
+                'date': cur_date.date(),
+                'count': count
+            })
+
+        return Response(count_list)
+
+
+class AdminLabelArticle(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        labels_queryset = Label.objects.all()
+        labels = []
+        for label in labels_queryset:
+            labels.append({
+                'category': label.name,
+                'count': label.article_set.all().count()  # 每个标签下的文章的数量
+            })
+        return Response(labels)
